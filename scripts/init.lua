eee9292b7ec5be29b79d260c46cbaa5a50932d4c
Regan Anderson <llisandur@outlook.com>
2025-06-13 00:35:09 -0500

    Rearrange files to Poptracker example pack, fix location data, fix item data

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,20 +1,49 @@
-Tracker:AddItems("items/items.json")
+-- entry point for all lua code of the pack
+-- more info on the lua API: https://github.com/black-sliver/PopTracker/blob/master/doc/PACKS.md#lua-interface
+ENABLE_DEBUG_LOG = true
+-- get current variant
+local variant = Tracker.ActiveVariantUID
+-- check variant info
+IS_ITEMS_ONLY = variant:find("itemsonly")
+IS_MAP = variant:find("maptracker")
 
-if (string.find(Tracker.ActiveVariantUID, "items_only")) then
+print("Loaded variant: ", variant)
+if ENABLE_DEBUG_LOG then
+  print("Debug logging is enabled!")
+end
+
+-- Utility Script for helper functions etc.
+ScriptHost:LoadScript("scripts/utils.lua")
+
+-- Logic
+ScriptHost:LoadScript("scripts/logic/logic.lua")
+
+-- Custom Items
+ScriptHost:LoadScript("scripts/custom_items/class.lua")
+ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlus.lua")
+ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlusWrapper.lua")
+
+-- Items
+Tracker:AddItems("items/items.jsonc")
+if IS_ITEMS_ONLY then
   Tracker:AddLayouts("layouts/tracker.json")
-elseif (string.find(Tracker.ActiveVariantUID, "map_tracker")) then
-  ScriptHost:LoadScript("scripts/logic.lua")
-  Tracker:AddMaps("map_tracker/maps/maps.json")
-  Tracker:AddLayouts("map_tracker/layouts/tracker.json")
-  Tracker:AddLocations("map_tracker/locations/locations.json")
+elseif IS_MAP then -- <--- use variant info to optimize loading
+  -- Maps
+  Tracker:AddMaps("var_maptracker/maps/maps.jsonc")
+  -- Locations
+  Tracker:AddLocations("var_maptracker/locations/locations.jsonc")
+  -- Tracker
+  Tracker:AddLayouts("var_maptracker/layouts/tracker.jsonc")
 else
   Tracker:AddLayouts("layouts/gatedTracker.json")
 end
 
-Tracker:AddLayouts("layouts/broadcast.json")
+-- Layout
+--Tracker:AddLayouts("layouts/items.jsonc")
+--Tracker:AddLayouts("layouts/tracker.jsonc")
+Tracker:AddLayouts("layouts/broadcast.jsonc")
 
-if _VERSION == "Lua 5.3" then
-    ScriptHost:LoadScript("scripts/autotracking.lua")
-else    
-    print("Auto-tracker is unsupported by your tracker version")
+-- AutoTracking for Poptracker
+if PopVersion and PopVersion >= "0.18.0" then
+  ScriptHost:LoadScript("scripts/autotracking.lua")
 end
